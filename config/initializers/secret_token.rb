c3061de08c3bf1d1f2488e731899052286d40523
Gwen Au <quyen_au@hotmail.com>
2014-09-04 17:56:56 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AlumniApp::Application.config.secret_token = '13b949573880f2c738d2700fc0f79b9a87f4c4e3525e37ef1b5fd52fc7ed099158fe1457642926a9ee96d6c3b8f3c442bcbb72e6ae8f7cd9f9a7b87cd6e51240'
