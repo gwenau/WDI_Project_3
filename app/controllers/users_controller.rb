a542a038e6aff2542ea8a3d79b551ab64d641ffe
Gwen Au <quyen_au@hotmail.com>
2014-09-05 14:29:06 +0100

    Reverted back to devise views only to avoid confusion with users view.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,4 @@
-class UserController < ApplicationController
+class UsersController < ApplicationController
   before_filter :authenticate_user!, except: :index
   # GET /users
   # GET /users.json
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ AlumniApp::Application.routes.draw do
 
   resources :messages
 
-
-  devise_for :users
+  # Might not need the code after :users because it looked like the transistion from login to logout was working sufficiently before.
+  devise_for :users, :path_names => { :sign_in => 'login', :sign_out => 'logout'}
 
   resources :events
 
