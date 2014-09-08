37226f490205847d75a1a0159029af5081e45f30
Gwen Au <quyen_au@hotmail.com>
2014-09-08 13:40:08 +0100

    Trial change of web host.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,7 +35,8 @@ AlumniApp::Application.configure do
   # Expands the lines which load the assets
   config.assets.debug = true
 
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  # config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.default_url_options = { host: 'http://lumnee-app.herokuapp.com/' }
 
   config.action_mailer.delivery_method = :smtp 
   config.action_mailer.perform_deliveries = true 
