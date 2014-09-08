c8ba7fc7fa50afe0a8b9c04a681ce45598e9e78e
Gwen Au <quyen_au@hotmail.com>
2014-09-08 14:03:30 +0100

    production settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,15 @@ AlumniApp::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
+  config.action_mailer.default_url_options = { host: 'lumnee-app.herokuapp.com' }
+
+  config.action_mailer.delivery_method = :smtp 
+  config.action_mailer.perform_deliveries = true 
+  config.action_mailer.raise_delivery_errors = true 
+  config.action_mailer.default :charset => "utf-8"
+
+  config.action_mailer.smtp_settings = { address: "smtp.mandrillapp.com", port: 25, enable_starttls_auto: true, user_name: ENV["MANDRILL_USERNAME"], password: ENV["MANDRILL_API_KEY"] }
+
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
