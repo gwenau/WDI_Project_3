bc46436beedafaafab0e24b88f27feb810dd43ce
Gwen Au <quyen_au@hotmail.com>
2014-09-09 12:12:01 +0100

    Temperature is now showing in degrees celsius.

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -5,8 +5,8 @@
 // $http to call on the api's.
   app.controller('LumneeController' , ["$scope", "$http", function($scope, $http) {
 
-      $http.get("http://api.openweathermap.org/data/2.5/weather?q=London,uk").success(function(data){
-        $scope.weatherData = data;
+      $http.get("http://api.openweathermap.org/data/2.5/weather?q=London,uk&mode=json&units=metric").success(function(data){
+        $scope.weatherData = data["main"]["temp"];
       })
 
   }])
