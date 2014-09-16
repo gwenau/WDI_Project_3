ad966722de7073ee9c190cd7592c7a0ab036845f
Gwen Au <quyen_au@hotmail.com>
2014-09-16 16:55:57 +0100

    Readded tree, we hope we found the error :-)

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,6 +1,6 @@
 (function() {
 
-  var app = angular.module('lumneeApp', ['ui.bootstrap', 'ngMap']]);
+  var app = angular.module('lumneeApp', ['ui.bootstrap', 'ngMap']);
  
 // $http to call on the api's.
   app.controller('lumneeController' , ["$scope", "$http", function($scope, $http) {
