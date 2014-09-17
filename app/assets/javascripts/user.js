be2db7f235b5e06e19dcbf9b4678f81d4741a97a
Gwen Au <quyen_au@hotmail.com>
2014-09-17 10:47:43 +0100

    Gon is not defined solution in layout application.

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -64,26 +64,9 @@
       });
 
 
-      // // Add Chat
-      // $scope.chatlines = [{username: "Mike", line: "Hello World"}];
-      // // $scope.other = [];
-
-      // $http.get('/chat.json').success(function(data) {
-      //   $scope.chatlines = data;
-      // })
-
-
-      // $scope.addlines = function (chat) {
-      //   console.log("addLinesFunctionFired.")
-      //   console.log($scope.chat)
-      //   $scope.chatlines.push($scope.chat)
-        
-        
-      //   $http.post('/chat.json', { chatline: $scope.chat}).success(function(data) {
-      //       $scope.chatlines.push(data)
-      //       $scope.chat = false        
-      //     })
-      // }
+      // // gon
+      // gon.preference = "hello"
+      // console.log(gon.preference)
 
 
   }])
